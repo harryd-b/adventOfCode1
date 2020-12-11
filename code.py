d66948226806f74bbe44f00b2caedd1bcc350abb
Harry <harry@d-b.co.uk>
2020-12-11 16:23:06 +0000

    Day 8 Parts 1&2 (cleanup)

--- code.py
+++ code.py
@@ -6,7 +6,7 @@ def load_and_boot():
     instruction_list = []
     path = 'venv/Include/code.txt'
     load_file_to_list(path, instruction_list)
-    print(boot(instruction_list))
+    boot(instruction_list)
 
 
 def boot_bug_finder():
@@ -20,24 +20,12 @@ def boot_bug_finder():
         test_instruction_list = copy.deepcopy(instruction_list)
         if instruction_list[i].find("jmp") >= 0:
             test_instruction_list[i] = test_instruction_list[i].replace("jmp", "nop")
-            print("changed from: ")
-            print(instruction_list[i])
-            print("to: ")
-            print(test_instruction_list[i])
-            print("at: ")
-            print(i)
         elif instruction_list[i].find("nop") >= 0:
             test_instruction_list[i] = test_instruction_list[i].replace("nop", "jmp")
-            print("changed from: ")
-            print(instruction_list[i])
-            print("to: ")
-            print(test_instruction_list[i])
-            print("at: ")
-            print(i)
         i += 1
-    #print()
-    print("Ended While Loop Position at: ")
+    print("Bug Found at Line: ")
     print(i)
+
 
 def boot(instructions):
     accumulator = 0
@@ -64,18 +52,20 @@ def boot(instructions):
                 if number_of_moves.isdigit():
                     position += int(number_of_moves)
                 else:
-                    print("Found a non-numeric number of moves at: ")
-                    print(position)
+                    break
             else:
                 position -= int(number_of_moves)
 
         if position in previous_positions:
             no_repeat = False
-
-
+    print("Accumulator = ")
+    print(accumulator)
     return no_repeat
 
 
 if __name__ == '__main__':
+    print("Load and Boot")
     load_and_boot()
-    #boot_bug_finder()
+    print("")
+    print("Bug Finder")
+    boot_bug_finder()
