7dec6e41599d74c36ee63e569a410535fa6c8fb9
Harry <harry@d-b.co.uk>
2020-12-16 08:26:06 +0000

    leftovers

--- xmas.py
+++ xmas.py
@@ -23,7 +23,7 @@ def loop_through_set(my_xmas_data):
 
 
 # Function to print sublist having given sum using Hashing
-def findSublist(A, my_sum):
+def findSublist(xmas_list, my_sum):
     # insert (0, -1) pair into the set to handle the case when
     # sublist with given sum starts from index 0
     dict = {0: -1}
@@ -32,17 +32,18 @@ def findSublist(A, my_sum):
     sum_so_far = 0
 
     # traverse the given list
-    for i in range(len(A)):
+    for i in range(len(xmas_list)):
 
         # update sum_so_far
-        sum_so_far += A[i]
+        sum_so_far += xmas_list[i]
 
-        # if (sum_so_far - sum) is seen before, we have found
-        # the sublist with sum 'sum'
+        # if (sum_so_far - my_sum) is seen before, we have found
+        # the sublist with sum 'my_sum'
         if (sum_so_far - my_sum) in dict:
-            contiguous_sub_array = copy.deepcopy(A[dict.get(sum_so_far - my_sum)+1:i+1])
+            contiguous_sub_array = copy.deepcopy(xmas_list[dict.get(sum_so_far - my_sum) + 1:i + 1])
             print("Sublist found", (dict.get(sum_so_far - my_sum) + 1, i))
             print(contiguous_sub_array)
+            print("Sum of contiguous sub_array")
             print(sum(contiguous_sub_array))
             print("Largest Number:")
             print(max(contiguous_sub_array))
@@ -60,8 +61,8 @@ if __name__ == '__main__':
     xmas_data = []
     path = 'venv/Include/xmas.txt'
     load_file_to_list(path, xmas_data)
-    # Part 1
+    print("Day 8: Part 1:")
     loop_through_set(xmas_data)
-
-    # Part 2
+    print("")
+    print("Day 8: Part 2:")
     findSublist(list(map(int, xmas_data)), 731031916)
